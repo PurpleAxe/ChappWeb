73117ee18b61caf03935cf879706cc35a0741250
PurpleAxe <doctormit768@gmail.com>
2019-12-31 16:11:40 +0200

    Initial Commit: Project setup

---
+++ src/main/java/com/visagie/chappweb/ChappwebApplication.java
@@ -0,0 +1,13 @@
+package com.visagie.chappweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChappwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChappwebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/visagie/chappweb/ChappwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visagie.chappweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChappwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
